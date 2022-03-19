d6e1fac4d0ff4853031dcb7a4c126232dcb163e3
Angie Siu <siuangie91@gmail.com>
2022-03-19 07:39:11 -0400

    feat: setup

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': ['warn', 'as-needed'],
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'import/no-cycle': 0,
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
+  },
+};
