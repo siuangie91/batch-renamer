283f1baf06f008e3cadadfaf11b69c6a836569ea
Angie Siu <siuangie91@gmail.com>
2022-03-20 12:44:33 -0400

    build: rebuild

--- build/utils/index.js
+++ build/utils/index.js
@@ -3,9 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.renameToNewFile = exports.createLeadingZeroes = void 0;
+exports.renameToNewFile = exports.createTargetFileName = exports.createLeadingZeroes = void 0;
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+/**
+ * Returns a string of zeroes, like "000" based on the total
+ * number of files and the index of the current file
+ * @param numFiles total number of files to rename
+ * @param index index of file to rename
+ * @returns a string of zeroes
+ */
 const createLeadingZeroes = (numFiles, index) => {
     // always have a leading zero
     if (numFiles < 10) {
@@ -21,12 +28,30 @@ const createLeadingZeroes = (numFiles, index) => {
     return leadingZeroes;
 };
 exports.createLeadingZeroes = createLeadingZeroes;
+/**
+ * Creates the file name to rename the file as
+ * @param props
+ * @returns new file name
+ */
+const createTargetFileName = ({ prefix, extension, numFiles, index, }) => {
+    const leadingZeroes = (0, exports.createLeadingZeroes)(numFiles, index);
+    return `${prefix}-${leadingZeroes}${index + 1}${extension}`;
+};
+exports.createTargetFileName = createTargetFileName;
+/**
+ * Renames a given file
+ * @param props
+ */
 const renameToNewFile = ({ originFolder, originalFile, targetFolder, numFiles, index, prefix, }) => {
     const extension = path_1.default.extname(originalFile);
     const basename = path_1.default.basename(originalFile, extension);
     console.log('🗂 ', basename, extension);
-    const leadingZeroes = (0, exports.createLeadingZeroes)(numFiles, index);
-    const targetFile = `${prefix}-${leadingZeroes}${index + 1}${extension}`;
+    const targetFile = (0, exports.createTargetFileName)({
+        prefix,
+        extension,
+        numFiles,
+        index,
+    });
     fs_1.default.copyFileSync(`${originFolder}/${originalFile}`, `${targetFolder}/${targetFile}`);
     console.log('🗳 ', targetFile);
 };
