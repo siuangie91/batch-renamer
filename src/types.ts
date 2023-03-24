3332cf05b61240f83f843d5fc2a604998614716b
Angie Siu <siuangie91@gmail.com>
2023-03-24 18:45:21 -0400

    style: format file

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,31 @@
 type TargetFileNameBaseParams = {
   prefix: string;
   startingIndex: number;
-}
+};
 
 type TargetPath = string | null;
 
 export type CommandArguments = TargetFileNameBaseParams & {
   origin: string;
   target: TargetPath;
-}
+};
 
 export type TargetFolder = {
   originFolderName: string;
   originParent: string;
   target: TargetPath;
-}
+};
 
 type TargetFileName = TargetFileNameBaseParams & {
-  index: number; 
-}
+  index: number;
+};
 
 export type TargetFile = TargetFileName & {
   extension: string;
-}
+};
 
 export type NewFileRenameProps = TargetFileName & {
   origin: string;
   originalFileName: string;
   targetFolderName: string;
-}
-
-// comment
+};
